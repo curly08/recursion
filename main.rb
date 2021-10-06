23257a8e7f2c6e74e72a3c4036a931cb4c093884
curly08 <mattynoakes08@gmail.com>
2021-10-06 15:30:06 -0400

    Add fibs_rec method

--- main.rb
+++ main.rb
@@ -5,12 +5,19 @@ def fibs(n)
     results.pop(2)
   when 1
     results.pop
-  when 2
-    results
   when 3..
-    n.times { results.push((results[-1] + results [-2])) }
+    (n - 2).times { results.push((results[-1] + results [-2])) }
   end
   results
 end
 
-p fibs(30)
+def fibs_rec(n)
+  return [0] if n == 1
+  return [0, 1] if n == 2
+  arr = fibs_rec(n - 1)
+  arr.push(arr[-1] + arr[-2])
+end
+
+p fibs(8)
+
+p fibs_rec(8)
